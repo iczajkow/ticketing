507d7eb3a36b8b4ad3df3fccaa2c04afc9288e02
Igor Czajkowski <igor.czajkowski@j-labs.pl>
2021-09-19 13:24:08 +0200

    Add getInitialProps

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,21 @@
-const LandingPage = () => {
-  return <h1>Landing Page1</h1>
-}
+import axios from "axios";
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
+
+  return <h1>Landing Page1</h1>;
+};
+
+LandingPage.getInitialProps = async () => {
+  if (typeof window === "undefined") {
+  ///Request on the server
+  } else {
+  //Request in the browser
+  }
+
+  const response = await axios.get("/api/users/currentuser");
+
+  return response.data;
+};
 
 export default LandingPage;
